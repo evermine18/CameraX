f34b0aac2dcb25cf94137c7a6cce9523c3108366
evermine18 <edgarsanou@gmail.com>
2023-01-17 17:11:36 +0100

    Trying to implement Take Picture Button

--- app/src/main/java/com/evermine/cameraxapp/CameraActivity.java
+++ app/src/main/java/com/evermine/cameraxapp/CameraActivity.java
@@ -5,6 +5,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
@@ -13,24 +15,71 @@ import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
 import android.util.Size;
 import android.view.OrientationEventListener;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class CameraActivity extends AppCompatActivity {
     private PreviewView previewView;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private TextView textView;
+    private Executor executor = Executors.newSingleThreadExecutor();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
         previewView = findViewById(R.id.previewView);
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        Button takePicture = findViewById(R.id.button2);
+        ImageCapture.Builder builder = new ImageCapture.Builder();
+        final ImageCapture imageCapture = builder
+                .setTargetRotation(this.getWindowManager().getDefaultDisplay().getRotation())
+                .build();
+        takePicture.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyyMMddHHmmss", Locale.US);
+               File file = new File(getFilesDir(), mDateFormat.format(new Date()) + ".jpg");
+
+               ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(file).build();
+               imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(getApplicationContext()), new ImageCapture.OnImageSavedCallback() {
+                   @Override
+                   public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                       new Handler().post(new Runnable() {
+                           @Override
+                           public void run() {
+                               Toast.makeText(CameraActivity.this, "Image Saved successfully", Toast.LENGTH_SHORT).show();
+                           }
+                       });
+                   }
+
+                   @Override
+                   public void onError(@NonNull ImageCaptureException error) {
+                       error.printStackTrace();
+                   }
+
+               });
+           }
+
+           ;
+       });
+
         cameraProviderFuture.addListener(new Runnable() {
             @Override
             public void run() {
